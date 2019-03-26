1ccb61a8bb3be979d676b87026e1f9dc78ad0445
coffey <coffey@nligo.com>
2019-03-26 16:56:38 +0800

    no message

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,11 +3,12 @@
 namespace Coffey\Weather\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class UserController extends Controller
 {
     public function index()
-    {
+   {
         return ['name' => 'coffey'];
     }
 }
